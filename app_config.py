63480d72e314b2a65adc30d428e8c4cb5f0a53ef
Sidhikajain1 <sidhikajain20@gmail.com>
2024-10-23 13:35:03 +0530

    Delete app_config.py

--- app_config.py
+++
@@ -1,6 +0,0 @@
-# app_config.py
-B2C_TENANT = "ssotesting9.onmicrosoft.com"  
-CLIENT_ID = "e112367c-6dbf-48bd-adb5-c259da425f81"    
-REDIRECT_URI = "https://sso-testing-dhd9apc7a5ehgafx.centralindia-01.azurewebsites.net/home"
-SIGN_UP_SIGN_IN_POLICY = "B2C_1_signupsignin"  
-AUTHORITY = f"https://ssotesting9.b2clogin.com/ssotesting9.onmicrosoft.com/B2C_1_signupsignin"
